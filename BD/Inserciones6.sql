14614d5cd200a363aed68923b30c3f5db20ca325
Isaac69694 <83610252+Isaac69694@users.noreply.github.com>
2022-03-24 23:43:01 -0600

    inserciones6

---
+++ BD/Inserciones6.sql
@@ -0,0 +1,6 @@
+
+
+INSERT INTO `Promociones`(`idPromocion`, `idProducto`, `nombre`, `descuento`, `canMin`,`canMax`) VALUES ('1','3','Verano','1200','2100');
+INSERT INTO `Promociones`(`idPromocion`, `idProducto`, `nombre`, `descuento`, `canMin`,`canMax`) VALUES ('2','4','Juebebes','800','1400');
+INSERT INTO `Promociones`(`idPromocion`, `idProducto`, `nombre`, `descuento`, `canMin`,`canMax`) VALUES ('3','1','4','Sabadrink','2000','2900');
+INSERT INTO `Promociones`(`idPromocion`, `idProducto`, `nombre`, `descuento`, `canMin`,`canMax`) VALUES ('4','5','3','Chikas','1000','1400');
